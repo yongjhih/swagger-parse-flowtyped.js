8565e97db381961a4087d69afc7da1e05df84971
Andrew Chen <yongjhih@gmail.com>
2018-10-18 22:46:27 +0800

    Support cli

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+#!/usr/bin/env node
 const parse = require('./lib/parse.js');
 const codegen = require('./lib/codegen.js');
-const fs = require('fs');
-const path = require('path');
-const jsonData = require('./swagger.json');
+const fs = require('fs-extra');
+const stdin = require('get-stdin');
 
-fs.writeFileSync(path.join(__dirname, 'swagger.js'), codegen(parse({
-    swagger: jsonData,
-    moduleName: 'swagger',
-    className: 'swagger'
-})));
+require('yargs')
+  .command('*', 'Generate', (yargs2) => {
+  }, (argv) => {
+      return stdin().then(str => JSON.parse(str)).then(str => {
+          console.log(str);
+          console.log(codegen(parse({
+              swagger: str,
+              moduleName: 'swagger',
+              className: 'swagger'
+          })));
+      });
+  })
+  .option('verbose', {
+    alias: 'v',
+    default: false
+  })
+  .option('h', {
+    alias: 'help',
+    description: 'display help message'
+  })
+  .help('help')
+  .fail((msg, err) => {
+    console.log(msg);
+    console.error(err);
+  }).argv;
